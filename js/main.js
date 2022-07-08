1d6d17e21367e58bc24cd7a7ab79a9e7c97ba64e
James Schoenecker <JamesSchoenecker62@gmail.com>
2022-07-07 21:03:58 -0600

    Added cors fix

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 document.querySelector('button').addEventListener('click',readInput)
+
+async function readInput(){
+    const name = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://simple-family-api-project.herokuapp.com/api/${name}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+    }catch(error){
+        console.log(error)
+    }
+}
